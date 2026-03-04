2012de68dc75371ab041fb1a14552721120ce792
Henry Anorue <hanorue@drohealth.com>
2026-03-04 14:09:12 +0100

    Register Router in main.py(Updating)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from app.core.database import engine, Base
 from app.models.users import User
+from app.api.routes import auth
+
 
 app = FastAPI(title="Appointment Booking API")
 
 Base.metadata.create_all(bind=engine)
-
-
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 @app.get("/")
 def root():
     return {"message": "API is running"}
