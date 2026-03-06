4aa7a07fde0774c2458e5c1faa42da51dd9b4de9
Henry Anorue <hanorue@drohealth.com>
2026-03-06 16:06:47 +0100

    Fixed some import error

--- app/api/deps.py
+++ app/api/deps.py
@@ -3,9 +3,9 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from jose import JWTError, jwt
 
-from app.core.database import get_db
+from app.core.dependencies import get_db
 from app.core.security import SECRET_KEY, ALGORITHM
-from app.models.user import User
+from app.models.users import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")   # Automatically reads: Authorization: Bearer <token>
 
