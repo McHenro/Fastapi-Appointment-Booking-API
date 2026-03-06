2a1ad2460dbeb5273019d41bbfb1333cd45ef4ba
Henry Anorue <hanorue@drohealth.com>
2026-03-06 16:01:48 +0100

    Create /me Route

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -5,6 +5,8 @@ from app.core.dependencies import get_db
 from app.models.users import User
 from app.schemas.user import UserCreate, UserLogin, Token
 from app.core.security import hash_password, verify_password, create_access_token
+from app.api.deps import get_current_user
+
 
 router = APIRouter()
 
@@ -45,3 +47,7 @@ def login(user_data: UserLogin, db: Session = Depends(get_db)):
         "access_token": access_token,
         "token_type": "bearer"
     }
+
+@router.get("/me")
+def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
