bda229c035e1c7552100c1a00faefe74b74259f8
Henry Anorue <hanorue@drohealth.com>
2026-03-09 11:52:26 +0100

    Create the Availability Model

---
+++ app/models/availability.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, Date, Time, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.core.database import Base
+
+class Availability(Base):
+    __tablename__ = "availabilities"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    doctor_id = Column(Integer, ForeignKey("users.id"))
+
+    date = Column(Date, nullable=False)
+
+    start_time = Column(Time, nullable=False)
+
+    end_time = Column(Time, nullable=False)
+
+    doctor = relationship("User")
