5d25adf3b8e1bd81c824302ad77a0aaed4c61c33
Henry Anorue <hanorue@drohealth.com>
2026-03-09 08:19:42 +0100

    Fix SQL Query Using Integer ID Instead of Email

--- app/api/deps.py
+++ app/api/deps.py
@@ -21,15 +21,15 @@ def get_current_user(
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: int = payload.get("sub")
+        user_email: str = payload.get("sub")
 
-        if user_id is None:
+        if user_email is None:
             raise credentials_exception
 
     except JWTError:
         raise credentials_exception
 
-    user = db.query(User).filter(User.id == user_id).first()
+    user = db.query(User).filter(User.email == user_email).first()
 
     if user is None:
         raise credentials_exception
